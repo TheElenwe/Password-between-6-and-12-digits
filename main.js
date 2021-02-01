19f337e72f35f4d04c4ead818188c1e8b88ed83b
unknown <you@example.com>
2021-02-01 23:22:11 +0200

    Second commit

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 const readlineSync = require("readline-sync");
 const userName = readlineSync.question("Input the Username: ");
-const profilPicture = readlineSync.question("Input the profil picture: ");
+const profilPicture = readlineSync.question("Has profil picture? ");
 const email = readlineSync.question("Input the email: ");
 count=0;
-if (userName === "elenwe") {
-} else if (profilPicture === "yes") {
-} else if (email === "elene97@hotmail.com") {
-} 
+
 while(true){
-    password = readlineSync.question("Input the password: ");
+ password = readlineSync.question("Input the password: ");
 if(password.length>=6 && password.length<=12){
     break;    
 }
